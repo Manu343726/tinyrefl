967dd6ffca6eba2c3ef7f5d706152941729a0b4d
Manu343726 <Manu343726@gmail.com>
2018-03-24 16:44:33 +0100

    Download LLVM release by default

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ git apply -v $SOURCE_DIR/stdlibcxx_checks.patch && git diff
 cd $SOURCE_DIR
 
 mkdir build && cd build
-cmake .. -DTINYREFL_BUILD_TESTS=ON
+cmake .. -DTINYREFL_BUILD_TESTS=ON -DTINYREFL_BUILD_EXAMPLES=ON
 make -j
 ctest . -V
 ./examples/tinyrefl-example
