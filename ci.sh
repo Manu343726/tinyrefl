f5ee9a0d894bb8023ee0f73896bfe46d935b5e2c
Manu343726 <Manu343726@gmail.com>
2018-03-27 13:05:36 +0200

    Remove type_safe patch from CI (Already merged)

--- ci.sh
+++ ci.sh
@@ -1,10 +1,5 @@
 #/bin/sh
 
-SOURCE_DIR=$(pwd)
-cd cppast/external/type_safe
-git apply -v $SOURCE_DIR/stdlibcxx_checks.patch && git diff
-cd $SOURCE_DIR
-
 mkdir build && cd build
 cmake .. -DTINYREFL_BUILD_TESTS=ON -DTINYREFL_BUILD_EXAMPLES=ON
 make -j
