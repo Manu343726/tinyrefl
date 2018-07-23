4649b78e21501d04b2bfe11967b61302b733d506
Manuel Sanchez <manuel@by.com.es>
2018-07-23 10:15:46 +0200

    Fix fmt include dir in the tool

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -1,5 +1,19 @@
 include(${TINYREFL_SOURCE_DIR}/cmake/DownloadProject.cmake)
 
+function(require_target NAME)
+    if(NOT TARGET ${NAME})
+        message(FATAL_ERROR "\"${NAME}\" is not a target")
+    else()
+        message(STATUS "Target \"${NAME}\" found")
+    endif()
+endfunction()
+
+function(require_targets)
+    foreach(name ${ARGN})
+        require_target(${name})
+    endforeach()
+endfunction()
+
 macro(external_dependency NAME URL COMMIT)
     if(NOT TARGET ${NAME})
         message(STATUS "external dependency ${NAME} from ${URL} at ${COMMIT}")
@@ -9,10 +23,7 @@ macro(external_dependency NAME URL COMMIT)
             GIT_TAG "${COMMIT}"
         )
         add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
-
-        if(NOT TARGET ${NAME})
-            message(FATAL_ERROR "Target ${NAME} not found after download")
-        endif()
+        require_target(${NAME})
     else()
         message(STATUS "external dependency ${NAME} already satisfied")
     endif()
