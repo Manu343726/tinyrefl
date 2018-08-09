f895007ac5601c0dccc55d73ed66d952be0c7381
Manu343726 <Manu343726@gmail.com>
2018-08-09 17:43:16 +0200

    Do not configure tool deps on host when cross building
    Also make sure tests are disabled when cross building

--- ci/ci.sh
+++ ci/ci.sh
@@ -7,15 +7,20 @@ else
     MAKE_CONCURRENCY="-j1"
 fi;
 
+if [[ -z "$SOURCE_DIR" ]]; then
+    SOURCE_DIR=".."
+fi
+
 mkdir build && cd build
 
 echo Cross building: $CROSS_BUILDING
+echo Source dir: $SOURCE_DIR
 
 if [ "$CROSS_BUILDING" == "YES" ]; then
-    cmake .. -DCMAKE_TOOLCHAIN_FILE="/usr/share/toolchain.cmake" -DTINYREFL_BUILD_EXAMPLES=ON
+    cmake $SOURCE_DIR -DCMAKE_TOOLCHAIN_FILE="/usr/share/toolchain.cmake" -DTINYREFL_BUILD_EXAMPLES=ON
     make $MAKE_CONCURRENCY VERBOSE=1
 else
-    cmake .. -DTINYREFL_BUILD_TESTS=ON -DTINYREFL_BUILD_EXAMPLES=ON
+    cmake $SOURCE_DIR -DTINYREFL_BUILD_TESTS=ON -DTINYREFL_BUILD_EXAMPLES=ON
     make $MAKE_CONCURRENCY VERBOSE=1
     ctest . -V
     ./examples/tinyrefl-example
