41d67e32274b3f74e9a267da1cc4a96a7fd2635e
Manu343726 <Manu343726@gmail.com>
2018-08-10 22:18:04 +0200

    Refoobarize ci script. Use ninja generator by default

--- ci/ci.sh
+++ ci/ci.sh
@@ -1,12 +1,6 @@
 #/bin/sh
 set -e
 
-if [[ "$CI_RUNNER_TAGS" = *"Manu343726-runner"* ]]; then
-    MAKE_CONCURRENCY="-j"
-else
-    MAKE_CONCURRENCY="-j1"
-fi;
-
 if [[ -z "$SOURCE_DIR" ]]; then
     SOURCE_DIR=".."
 fi
@@ -15,18 +9,29 @@ if [[ -z "$LLVM_VERSION" ]]; then
     LLVM_VERSION="4.0.0"
 fi
 
+if [[ -z "$GENERATOR" ]]; then
+    GENERATOR=Ninja
+fi
+
 mkdir build && cd build
 
 echo Cross building: $CROSS_BUILDING
 echo Source dir: $SOURCE_DIR
 echo LLVM version: $LLVM_VERSION
+echo Generator: $GENERATOR
 
 if [ "$CROSS_BUILDING" == "YES" ]; then
-    cmake $SOURCE_DIR -DCMAKE_TOOLCHAIN_FILE="/usr/share/toolchain.cmake" -DTINYREFL_LLVM_VERSION="${LLVM_VERSION}" -DTINYREFL_LLVM_DOWNLOAD_FROM_OFFICIAL_SERVER="ON"
-    make $MAKE_CONCURRENCY VERBOSE=1
+    toolchain="-DCMAKE_TOOLCHAIN_FILE=\"/usr/share/toolchain.cmake\""
 else
-    cmake $SOURCE_DIR -DTINYREFL_LLVM_VERSION="${LLVM_VERSION}" -DTINYREFL_LLVM_DOWNLOAD_FROM_OFFICIAL_SERVER="ON"
-    make $MAKE_CONCURRENCY VERBOSE=1
-    ctest . -V
-    ./examples/tinyrefl-example
+    toolchain=""
 fi
+
+cmake $SOURCE_DIR -G $GENERATOR \
+        $toolchain \
+        -DTINYREFL_LLVM_VERSION="${LLVM_VERSION}" \
+        -DTINYREFL_LLVM_DOWNLOAD_FROM_OFFICIAL_SERVER="ON" \
+        -DCMAKE_VERBOSE_MAKEFILE=ON
+
+cmake --build .
+ctest . -V
+./examples/tinyrefl-example
