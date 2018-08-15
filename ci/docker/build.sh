79a9459b3f63d72b9677b912b72d803231275176
Manu343726 <Manu343726@gmail.com>
2018-08-15 19:39:52 +0200

    Install boost in all ci images
    Needed to build and run boost backend examples

--- ci/docker/build.sh
+++ ci/docker/build.sh
@@ -3,7 +3,7 @@
 REPO="manu343726"
 NAME="tinyrefl"
 
-docker build --tag ${REPO}/${NAME}:clang50-x86 docker/clang50-x86
+docker build --tag ${REPO}/${NAME}:clang50-x86 docker/clang-50-x86
 docker build --tag ${REPO}/${NAME}:gcc5-x86 docker/gcc-5-x86
 docker build --tag ${REPO}/${NAME}:gcc6-x86 docker/gcc-6-x86
 docker build --tag ${REPO}/${NAME}:gcc7-x86 docker/gcc-7-x86
