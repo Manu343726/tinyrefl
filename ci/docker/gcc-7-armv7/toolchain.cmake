a7042889f1bf3ac40d70e3314be51ead3311198c
Manu343726 <Manu343726@gmail.com>
2018-08-15 23:33:14 +0200

    Fix clang ci container builds
    The clang container was not being built thanks to
    a bug in the build script.
    
    We also changed the container base to a conan image,
    and changed the tool/externals/CMakeScripts.txt to always build
    with the old glibcxx ABI (llvm releases are built with the old ABI)

--- ci/docker/gcc-7-armv7/toolchain.cmake
+++ ci/docker/gcc-7-armv7/toolchain.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabi-g++)
+set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabi-gcc-7)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabi-g++-7)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
