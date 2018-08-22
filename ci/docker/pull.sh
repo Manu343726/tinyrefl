f8ce07f811af1e12bddd43cf7c99a8374545c02c
Manuel Sanchez <manuel@by.com.es>
2018-08-22 14:35:15 +0200

    Add script to pull all docker CI images

---
+++ ci/docker/pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker pull manu343726/tinyrefl:clang50-x86
+docker pull manu343726/tinyrefl:gcc5-armv7
+docker pull manu343726/tinyrefl:gcc5-x86
+docker pull manu343726/tinyrefl:gcc6-armv7
+docker pull manu343726/tinyrefl:gcc6-x86
+docker pull manu343726/tinyrefl:gcc7-armv7
+docker pull manu343726/tinyrefl:gcc7-x86
