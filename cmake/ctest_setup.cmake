2ce2a975cb0743adacef851e764de135fcf0c156
Manu343726 <Manu343726@gmail.com>
2018-10-05 09:01:34 +0200

    Configure valgrind max backtrace depth

--- cmake/ctest_setup.cmake
+++ cmake/ctest_setup.cmake
@@ -4,9 +4,13 @@ function(add_tinyrefl_test NAME)
 
     find_program(MEMORYCHECK_COMMAND valgrind)
 
+    if(NOT TINYREFL_VALGRIND_MAX_BACKTRACE_DEPTH)
+        set(TINYREFL_VALGRIND_MAX_BACKTRACE_DEPTH 100)
+    endif()
+
     if(MEMORYCHECK_COMMAND)
         message(STATUS "Running CTest ${NAME} with valgrind")
-        add_test(NAME ${NAME} COMMAND ${MEMORYCHECK_COMMAND} --leak-check=full $<TARGET_FILE:${NAME}>)
+        add_test(NAME ${NAME} COMMAND ${MEMORYCHECK_COMMAND} --leak-check=full --num-callers=${TINYREFL_VALGRIND_MAX_BACKTRACE_DEPTH} $<TARGET_FILE:${NAME}>)
     else()
         add_test(${NAME} ${NAME})
     endif()
