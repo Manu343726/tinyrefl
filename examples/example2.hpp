882f8a0a2c31bd4914969e27b77beb6c319a2206
Manuel Sanchez <manuel@by.com.es>
2019-03-25 08:48:15 +0100

    Run clang format on new sources

--- examples/example2.hpp
+++ examples/example2.hpp
@@ -35,13 +35,12 @@ public:
     B                                subobject;
 
     C() = default;
-    explicit C(const std::string& str) {}
-    [[C]] C(int a, int b) {}
+    explicit C(const std::string& str){}[[C]] C(int a, int b){}
 
-    [[f, f1, f2]] void f(int a, int b) const {};
-    [[f]] void         f(int a, int b){};
-    [[f]] void         f(){};
-    [[f]] void         f() const {};
+        [[ f, f1, f2 ]] void f(int a, int b) const {};
+    [[f]] void f(int a, int b) {};
+    [[f]] void f() {};
+    [[f]] void f() const {};
 
     enum class Enum
     {
